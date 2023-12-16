f639e14eafec0748c53c6c64824505918a4d0d60
gado321 <21521955@gm.uit.edu.vn>
2023-12-16 20:28:47 +0700

    update huhu

--- client/src/PageLoading/PageLoading.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import img from "";
-const PageLoading = () => {
-    return (
-        <img src= ".\src\PageLoading\image_1.png" alt="loadingpage" />
-    );
-}
-
-export default PageLoading;
