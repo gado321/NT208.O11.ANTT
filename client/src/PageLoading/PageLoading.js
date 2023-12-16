b4387c26cfa4104ccac6ad3cce789f74a9ec63d7
gado321 <21521955@gm.uit.edu.vn>
2023-12-16 17:34:18 +0700

    update something

---
+++ client/src/PageLoading/PageLoading.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import img from "";
+const PageLoading = () => {
+    return (
+        <img src= ".\src\PageLoading\image_1.png" alt="loadingpage" />
+    );
+}
+
+export default PageLoading;
